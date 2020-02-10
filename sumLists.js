5db31c6c4dd752e78caa750151efe563fcb773d9
jkeane889 <jkeane889@gmail.com>
2020-02-09 18:25:48 -0600

    SumList linkedList practice

--- sumLists.js
+++ sumLists.js
@@ -1,4 +1,13 @@
-// Sum Lists
+/*
+
+Sum Lists
+
+You have two numbers represented by a linked list, where each node contains a single
+digit. The digits are stored in reverse order, such that the 1 's digit is at the head of the list. 
+
+Write a function that adds the two numbers and returns the sum as a linked list
+
+*/
 
 // I - two numbers represented by a linked list, with node's value
 //      representing digit of number in reverse order
@@ -10,109 +19,114 @@ const LinkedList = () => {
     var list = {};
     list.head = null;
     list.tail = null;
-  
+
     list.addToTail = (value) => {
-      let newTail = Node(value);
-      
-      if (!list.head) {
+    let newTail = Node(value);
+    
+    if (!list.head) {
         list.head = newTail;
-      };
-  
-      if (list.tail) {
+    };
+
+    if (list.tail) {
         list.tail.next = newTail;
-      };
-  
-      list.tail = newTail;
     };
-  
+
+    list.tail = newTail;
+    };
+
     list.removeHead = () => {
-      if (list.head === null) {
+    if (list.head === null) {
         return null
-      }
-  
-      let currentHead = list.head;
-      list.head = list.head.next;
-  
-      return currentHead.value;
-    };
-  
-    list.contains = (target) => {
-      let node = list.head;
-  
-      while (node) {
-        if (node.value === target) {
-          return true
-        }
-  
-        node = node.next
-      }
-  
-      return false
-    };
-  
-    return list;
-};
-  
-  var Node = function(value) {
-    var node = {};
-  
-    node.value = value;
-    node.next = null;
-  
-    return node;
-};
-  
-/*
-
-    create helper function to add items from linkedList (input is a single LinkedList)
-        create digitString to store digits from LinkedList
-
-        let node = linkedList.head
-
-        while (node.next) {
-            string.unshift(node.data)
-        }
-
-        return digitString
-
-    create reversedLinkedList = (sum) => {
-        let stringArray = sum.toString().split('');
-        let linkedList = {};
-        
-        while (stringSum.length) {
-            let char = stringSum.pop();
-            let node = {};
-            
-            if (!linkedList.head) {
-                let node.data = char;
-                let node.next = null;
-            }
-
-            node.data = char;
-            node.next = linkedList.head;
-            let lastNode = linkedList.head;
-            linkedList.head = node;
-        }
-
-        return linkedList
     }
 
-    create function to add linkedLists from parameters
-        let reversedDigitStringOne = helper function (linkedListOne); 
-        let reversedDigitStringTwo = helper function (linkedListTwo);
-        
-        if (reversedDigitStringTwo.length < reversedDigitStringOne.length) {
-            reversedDigitStringTwo.unshift(0);
-        } else if (reversedDigitStringOne.length < reversedDigitStringTwo.length) {
-            reversedDigitStringOne.unshift(0);
+    let currentHead = list.head;
+    list.head = list.head.next;
+
+    return currentHead.value;
+    };
+
+    list.contains = (target) => {
+    let node = list.head;
+
+    while (node) {
+        if (node.value === target) {
+        return true
         }
 
-        let parsedStringOne = parseInt(reversedDigitStringOne);
-        let parsedStringTwo = parseInt(reversedDigitStringTwo);
+        node = node.next
+    }
 
-        let sum = parsedStringOne + parsedStringTwo;
+    return false
+    };
 
-        return reversedLinkedList(sum)
+    return list;
+};
 
-*/
+var Node = function(value) {
+    var node = {};
 
+    node.value = value;
+    node.next = null;
+
+    return node;
+};
+
+linkedListOne = LinkedList();
+linkedListTwo = LinkedList();
+
+linkedListOne.addToTail(7);
+linkedListOne.addToTail(1);
+linkedListOne.addToTail(6);
+
+linkedListTwo.addToTail(5);
+linkedListTwo.addToTail(9);
+linkedListTwo.addToTail(2);
+
+console.log("This is the first linkedList: ", linkedListOne);
+console.log("This is the second linkedList: ", linkedListTwo);
+
+const sumLists = (list1, list2) => {
+    let reversedStringOne = createString(list1);
+    let reversedStringTwo = createString(list2);
+
+    if (reversedStringOne.length < reversedStringTwo.length) {
+        reversedStringOne.unshift('0');
+    } else if (reversedStringTwo.length < reversedStringOne.length) {
+        reversedStringTwo.unshift('0');
+    }
+
+    let parsedStringOne = parseInt(reversedStringOne);
+    let parsedStringTwo = parseInt(reversedStringTwo);
+
+    let sum = parsedStringOne + parsedStringTwo;
+    let sumList = reversedLinkedList(sum);
+    
+    return sumList;
+}
+
+const reversedLinkedList = (str) => {
+    let stringArray = str.toString().split('');
+    let newLinkedList = LinkedList();
+
+    while (stringArray.length) {
+        let char = stringArray.pop();
+        newLinkedList.addToTail(char);
+    }
+
+    return newLinkedList
+}
+
+const createString = (list) => {
+    let digitString = [];
+    let node = list.head;
+
+    while (node) {
+        let nodeStr = node.value.toString();
+        digitString.unshift(nodeStr);
+        node = node.next;
+    }
+
+    return digitString.join('')
+};
+
+sumLists(linkedListOne, linkedListTwo);
